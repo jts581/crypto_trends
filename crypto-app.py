dfec73ae68c4dd3a7cbfd22bb266b65e2f0f0b49
LLC314 <84344987+LLC314@users.noreply.github.com>
2021-06-24 10:34:29 -0400

    Entered Crypto List

--- crypto-app.py
+++ crypto-app.py
@@ -4,11 +4,13 @@ from dotenv import load_dotenv
 
 # retrieve API Key from env file
 load_dotenv()
+
 NYT_API_KEY = os.getenv("NYT_API_KEY")
 
 
 
-filter_query = input("Please enter a cryptocurrency: ")
+filter_query = input("Please enter a cryptocurrency from this list: Bitcoin, Etherium, Dogecoin, Cardano, Ripple")
 
 # Article Search API
-request_url = https://api.nytimes.com/svc/search/v2/articlesearch.json?q=cryptocurrency&fq={filter_query}&api-key={NYT_API_KEY}
+request_url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=cryptocurrency&fq={filter_query}&api-key={NYT_API_KEY}"
+
