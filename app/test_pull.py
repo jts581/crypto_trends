aca2557ae07c36a631a6811abd0857a24b96b814
jts581 <jts581@stern.nyu.edu>
2021-06-24 19:21:39 -0400

    Switched to article search API

--- app/test_pull.py
+++ app/test_pull.py
@@ -1,4 +1,6 @@
+import datetime
 import os
+import pandas as pd
 from re import search
 from typing import Counter
 from dotenv import load_dotenv
@@ -6,12 +8,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 ## Requesting API from user, similar to entering login information
-
-#from getpass import getpass 
-#API_KEY = getpass("API Key: ") 
-# Note that entering an API in terminal (at least Git Bash) is difficult
-# ...because user can't see what they're entering.
-# ...see here for details: https://stackoverflow.com/questions/13399315/cant-type-password-in-git-bash
 
 API_KEY = os.getenv("API_KEY")
 print(len(API_KEY))
@@ -34,15 +30,27 @@ pprint([method for method in dir(client) if not method.startswith("_")])
 ## ...top_stories can be filtered by section, most_viewed and most_shared can't
 ## ...and we'll filter top_stories by the "business" section    
 
-biz_stories = client.top_stories(section="business")
-(len(biz_stories))
+## Set a variable for current day
+current_date = datetime.datetime.today()
+print(current_date)
+
+start_date = datetime.date(current_date)
+number_of_days = 5
+date_list = []
+for day in range(number_of_days):
+  a_date = (start_date + datetime.timedelta(days = day)).isoformat()
+  date_list.append(a_date)
+
+stories = client.article_search(dates= {date_list})
+
+(len(stories))
 
 ## Return the first story, its type (it's a dict), and its keys
 
-story = biz_stories[0]
+story = stories[0]
 pprint(type(story))
 pprint(story.keys())
-#pprint(story)
+#pprint(story)S
 
 ## Return the title, abstract, des_facet, and org_facet
 #pprint(story["title"])
@@ -53,12 +61,14 @@ pprint(story.keys())
 ## Return the titles of articles that have a certain word in their
 ## ...title, abstract, des_facet, or org_facet
 
-search_phrase = "pandemic" #> Just a thought, we could even turn this into a user input
+search_phrase = "bitcoin" #> Just a thought, we could even turn this into a user input
 
 filtered_stories = []
 for item in biz_stories:
     if search_phrase in (item["abstract"] or item["title"] or item["des_facet"] or item["org_facet"]):
         filtered_stories.append(item)
+#> Is this or statement working?
+#> Is this double counting?
 
 print(len(filtered_stories))
 
