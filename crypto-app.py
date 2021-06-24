40efce96411931ea9c34b30cc4dda1f09e339563
LLC314 <84344987+LLC314@users.noreply.github.com>
2021-06-24 10:44:19 -0400

    Spacing Edit

--- crypto-app.py
+++ crypto-app.py
@@ -9,7 +9,7 @@ NYT_API_KEY = os.getenv("NYT_API_KEY")
 
 
 
-filter_query = input("Please enter a cryptocurrency from this list: Bitcoin, Etherium, Dogecoin, Cardano, Ripple")
+filter_query = input("Please enter a cryptocurrency from this list: Bitcoin, Etherium, Dogecoin, Cardano, Ripple -- ")
 
 # Article Search API
 request_url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=cryptocurrency&fq={filter_query}&api-key={NYT_API_KEY}"
