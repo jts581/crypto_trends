c68466a04f17196c3e6b4a1ba601a606907336ac
LLC314 <84344987+LLC314@users.noreply.github.com>
2021-06-24 09:41:35 -0400

    Setup

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # freestyle_project
 Crypto trends project
+
+Navigate to the directory
+cd~ /Desktop/freestyle_project
+
+Setup the environment
+conda create -n crypto-env python 3.8
+conda activate cypto-env
+
+Install the required packages
+pip install -r requirements.txt
+
+Run the python script
+python crypto-app.py
---
+++ crypto-app.py
@@ -0,0 +1,14 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+# retrieve API Key from env file
+load_dotenv()
+NYT_API_KEY = os.getenv("NYT_API_KEY")
+
+
+
+filter_query = input("Please enter a cryptocurrency: ")
+
+# Article Search API
+request_url = https://api.nytimes.com/svc/search/v2/articlesearch.json?q=cryptocurrency&fq={filter_query}&api-key={NYT_API_KEY}
